3612d56dc50827dd90c88593dc95856ebfba5e4f
ubaaiiii <rizqi.ubaidillah15@mhs.ubharajaya.ac.id>
2023-07-15 22:32:02 +0700

    init before theme

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
-import './style.css'
 import './index.css'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import store from './store'
+import router from './router'
+
+
+createApp(App)
+.use(store)
+.use(router)
+.mount('#app')
